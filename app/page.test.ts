d4d08200cba7feb2565ce79b6de5b8118c70857e
kabradshaw1 <itllbok82@gmail.com>
2024-04-08 15:14:48 -0400

    working on page

--- app/page.test.ts
+++ app/page.test.ts
@@ -1,11 +1,4 @@
-import { setupServer } from 'msw/node';
-import { http, graphql, HttpResponse } from 'msw'
-
 describe('page', () => {
-  const server = setupServer(
-    
-  );  
-  beforeAll(() => server.listen());
 
   it('should test the page', async () => {
     const value = 1;
