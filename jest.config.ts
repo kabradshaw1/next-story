509dc21c2d3b6e59c37b71f29b6d1a56a10f5618
kabradshaw1 <itllbok82@gmail.com>
2024-04-08 12:05:32 -0400

    working on test

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,16 @@
 import { Config } from '@jest/types';
 const config: Config.InitialOptions = {
-  preset: 'ts-jest', // Use 'jsdom' for browser-like environments
-  testEnvironment: 'ts-node', 
-  
-};
-module.exports = {
+  roots: ['<rootDir>'],
   preset: 'ts-jest',
   testEnvironment: 'node', // Use 'jsdom' for browser-like environments
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], // Jest setup file
+  moduleNameMapper: {
+    // Handle module aliases (if you're using them in your project)
+    '^@components/(.*)$': '<rootDir>/components/$1',
+  },
+  transform: {
+    // Use babel-jest to transpile tests with the next/babel preset
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
 };
+export default config;
