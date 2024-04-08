848c598a3a3bfe99ba85a51d7d5d83b49f644950
kabradshaw1 <itllbok82@gmail.com>
2024-04-08 14:32:59 -0400

    trying to gets testing enviroment to work with msw

--- app/page.test.ts
+++ app/page.test.ts
@@ -1,17 +1,12 @@
-import { http, HttpResponse } from 'msw';
 import { setupServer } from 'msw/node';
+import { http, graphql, HttpResponse } from 'msw'
 
 describe('page', () => {
   const server = setupServer(
-    http.post('http://localhost:8080/api/login', ({ request, params, cookies }) => {
-      return HttpResponse.json(
-        { accessToken: "mockedAccessToken" },
-        { status: 200 } // Ensure to send the correct HTTP status code for success
-      );
-    }),
+    
   );  
   beforeAll(() => server.listen());
-  
+
   it('should test the page', async () => {
     const value = 1;
     expect(value).toBe(1);
--- jest.config.ts
+++ jest.config.ts
@@ -9,15 +9,28 @@ const customJestConfig = {
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFiles: ['<rootDir>/jest.polyfills.ts'],
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/cypress/'],
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.tsx?$': '@swc/jest',
   },
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.test.json'
     }
+  },
+  testEnvironmentOptions: {
+    /**
+     * @note Opt-out from JSDOM using browser-style resolution
+     * for dependencies. This is simply incorrect, as JSDOM is
+     * not a browser, and loading browser-oriented bundles in
+     * Node.js will break things.
+     *
+     * Consider migrating to a more modern test runner if you
+     * don't want to deal with this.
+     */
+    customExportConditions: [''],
   },
 };
 
