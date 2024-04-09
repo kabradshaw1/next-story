84655468e437f7b04fa44e6e8ededfeb6ff3c072
kabradshaw1 <itllbok82@gmail.com>
2024-04-09 16:36:00 -0400

    working on basics of next

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+import Logo from "@/component/Logo/Logo";
+
 export default function Home() {
   return (
     <main>
+      <div className="w-full max-w-xs">
+        <div className="bg-gray-800 shadow-md rounded-lg px-8 pt-6 pb-8 mb-4">
+          <Logo/>
+        </div>
+      </div>
     </main>
   );
 }
