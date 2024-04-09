1d8dad05cef9cca1e85571de944e974ed048aff2
kabradshaw1 <itllbok82@gmail.com>
2024-04-09 16:59:27 -0400

    working on the things

--- component/Logo/Logo.tsx
+++ component/Logo/Logo.tsx
@@ -3,12 +3,20 @@ import Image from "next/image";
 const Logo: React.FC = () => {
  return(
   <div style={{
-    width: '100%',
-    height: '400px',
+    width: '250px',
+    height: '250px',
     position: 'relative',
     overflow: 'hidden'
   }}>
-    <Image src="/images/LOGO.png" alt="Logo Image" layout="fill" objectFit="cover" />
+    <Image src="/images/LOGO.png" alt="Logo Image" layout="fill" style={{
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      objectFit: 'contain',
+      left: '50%',
+      top: '50%',
+      transform: 'translate(-50%, -50%)'
+    }}/>
   </div>
  )
 }
@@ -25,4 +33,4 @@ style={{
   transform: 'translate(-50%, -50%)'
 }}
 /> */
-export default Logo;
+export default Logo; 
