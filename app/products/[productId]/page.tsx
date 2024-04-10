b7208fca45aeb233b54fe815c538af4006b5b9b3
kabradshaw1 <itllbok82@gmail.com>
2024-04-10 14:09:12 -0400

    tutorial

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,5 +1,23 @@
 import { notFound } from "next/navigation"
-export default function ProductDetails( { params }: { params: { productId: string } } ) {
+import { Metadata } from "next"
+
+type Props = {
+  params: {
+    productId: string  
+  }
+}
+
+export const generaeteMatadata = async ({ params }: Props): Promise<Metadata> => {
+  const title = await new Promise(resolve => {
+    setTimeout(() => {
+      resolve(`stuff ${params.productId}`)
+    }, 100)
+  })
+  return {
+    title: `Product ${params.productId}`
+  }
+}
+export default function ProductDetails( { params }: Props ) {
   
   return (
     <div>
