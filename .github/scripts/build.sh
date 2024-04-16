98cdc806e947d94fba950741853e27e22f1d517b
kabradshaw1 <itllbok82@gmail.com>
2024-04-16 09:40:22 -0400

    working on deploying

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -5,8 +5,10 @@ set -xe
   
   docker-compose -f docker-compose.prod.yml down
 
+  docker image prune -a   
+
   git pull origin main
 
   docker-compose -f docker-compose.prod.yml up -d --build
 
-  docker image prune -a -f --filter "until=24h"
+  
