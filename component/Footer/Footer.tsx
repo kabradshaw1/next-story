584bbf5cc7aaac76a66814743bf8ebe2d1b1d99c
kabradshaw1 <itllbok82@gmail.com>
2024-04-15 22:46:09 -0400

    working on dev env

--- component/Footer/Footer.tsx
+++ component/Footer/Footer.tsx
@@ -1,7 +1,14 @@
+import { FaTwitter, FaFacebook, FaEnvelope } from 'react-icons/fa';
+
 export default function Footer() {
   return (
     <footer>
       <h1>Footer</h1>
+      <div>
+        <a href="https://twitter.com" aria-label="Twitter"><FaTwitter /></a>
+        <a href="https://facebook.com" aria-label="Facebook"><FaFacebook /></a>
+        <a href="mailto:example@example.com" aria-label="Mail"><FaEnvelope /></a>
+      </div>
     </footer>
-  )
-} 
+  );
+}
--- component/Header/Header.tsx
+++ component/Header/Header.tsx
@@ -6,12 +6,12 @@ import React from "react";
 export default function Header() {
 
   const pathname = usePathname() || '';
-  console.log(pathname);
+
   return (
     <header>
-      <Link className={pathname.startsWith('/') ? 'font-bold custom-blue' : 'dark-gray'} href={"/"}>Galaxy Voyagers</Link>
-      <Link className={pathname.startsWith('/login') ? 'font-bold custom-blue' : 'dark-gray'}href={"/login"}>Login</Link>
-      <Link className={pathname.startsWith('/register') ? 'font-bold custom-blue' : 'dark-gray'} href={"/register"}>Register</Link>
+      <Link className={pathname.startsWith('/') ? 'font-bold custom-blue' : 'dark-gray'} href={"/"}>Galaxy Voyagers </Link>
+      <Link className={pathname.startsWith('/login') ? 'font-bold custom-blue' : 'dark-gray'}href={"/login"}>Login </Link>
+      <Link className={pathname.startsWith('/register') ? 'font-bold custom-blue' : 'dark-gray'} href={"/register"}>Register </Link>
     </header>
   )
 }
