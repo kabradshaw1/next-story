4b332fba4e64615c3757d152c7bedbf7c9711eea
kabradshaw1 <itllbok82@gmail.com>
2024-05-03 03:03:14 +0000

    removing apollo

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,9 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Header />
-        <main>{children}</main>
+        <main className="flex flex-wrap justify-center items-center h-screen bg-dark-gray">
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -4,7 +4,6 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 import Logo from "@/components/Logo/Logo";
-import ClientWrapper from "@/utils/ClientWrapper";
 
 export default function Login() {
   const validationSchema = z.object({
@@ -30,56 +29,54 @@ export default function Login() {
   });
 
   return (
-    <ClientWrapper>
-      <main className="flex flex-wrap justify-center items-center h-screen bg-dark-gray">
-        <div className="w-full max-w-lg">
-          <form
-            noValidate
-            className="card"
-            onSubmit={handleSubmit((data) => console.log(data))}
-          >
-            <div className="mb-4">
-              <label htmlFor="email" className="label">
-                Email
-              </label>
-              <input
-                id="email"
-                type="email"
-                placeholder="Enter email"
-                {...register("email")}
-                className={`input ${errors.email ? "border-red-600" : "border-gray-700"} bg-gray-700`}
-                onBlur={() => trigger("email")}
-              />
-              <p className="error-message">{errors.email?.message}</p>
-            </div>
-            <div className="mb-6">
-              <label htmlFor="password" className="label">
-                Password
-              </label>
-              <input
-                id="password"
-                type="password"
-                placeholder="Enter password"
-                {...register("password")}
-                className={`input ${errors.password ? "border-red-600" : "border-gray-700"} bg-gray-700`}
-                onBlur={() => trigger("password")}
-              />
-              <p className="error-message">{errors.password?.message}</p>
-            </div>
-            <div className="flex items-center justify-between">
-              <button type="submit" className="btn">
-                Submit Form
-              </button>
-              <p className="text-sm text-custom-blue">place holder message</p>
-            </div>
-          </form>
-        </div>
-        <div className="w-full max-w-xs">
-          <div className="card">
-            <Logo />
+    <>
+      <div className="w-full max-w-lg">
+        <form
+          noValidate
+          className="card"
+          onSubmit={handleSubmit((data) => console.log(data))}
+        >
+          <div className="mb-4">
+            <label htmlFor="email" className="label">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              placeholder="Enter email"
+              {...register("email")}
+              className={`input ${errors.email ? "border-red-600" : "border-gray-700"} bg-gray-700`}
+              onBlur={() => trigger("email")}
+            />
+            <p className="error-message">{errors.email?.message}</p>
           </div>
+          <div className="mb-6">
+            <label htmlFor="password" className="label">
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              placeholder="Enter password"
+              {...register("password")}
+              className={`input ${errors.password ? "border-red-600" : "border-gray-700"} bg-gray-700`}
+              onBlur={() => trigger("password")}
+            />
+            <p className="error-message">{errors.password?.message}</p>
+          </div>
+          <div className="flex items-center justify-between">
+            <button type="submit" className="btn">
+              Submit Form
+            </button>
+            <p className="text-sm text-custom-blue">place holder message</p>
+          </div>
+        </form>
+      </div>
+      <div className="w-full max-w-xs">
+        <div className="card">
+          <Logo />
         </div>
-      </main>
-    </ClientWrapper>
+      </div>
+    </>
   );
 }
