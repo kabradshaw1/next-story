9a83602ccf4a76019507dfefccb87b03b7e68735
kabradshaw1 <itllbok82@gmail.com>
2024-05-04 22:17:12 +0000

    working on character

---
+++ app/character/Characters.tsx
@@ -0,0 +1,34 @@
+import React from "react"; // Ensure React is imported if using JSX
+
+import { Character } from "@prisma/client";
+import { useRouter } from "next/navigation";
+
+interface CharactersProps {
+  characters: Character[];
+}
+
+const Characters: React.FC<CharactersProps> = ({ characters }) => {
+  const router = useRouter();
+  const route = (characterId: number) => () =>
+    router.push(`/character/${characterId}`);
+
+  return (
+    <>
+      {characters.length > 0 ? (
+        characters.map((character) => (
+          <div key={character.id} className="card">
+            <h1>{character.title}</h1>
+            <p>{character.text || "No text available"}</p>
+            <button className="btn" onClick={route(character.id)}>
+              View Details
+            </button>
+          </div>
+        ))
+      ) : (
+        <div>No characters found.</div>
+      )}
+    </>
+  );
+};
+
+export default Characters;
---
+++ app/character/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import prisma from "@/prisma/prismaClient";
+
+import Characters from "./Characters";
+
+
+const CharactersContainer: React.FC = async () => {
+  const characters = await prisma.character.findMany({});
+  return (
+    <div className="w-full max-w-lg">
+      <div className="card">
+        <h1>Characters</h1>
+        <Characters characters={characters} />
+      </div>
+    </div>
+  );
+};
+
+export default CharactersContainer;
