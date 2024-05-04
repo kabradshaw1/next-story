608b2f80871691570031066e3e5494efa52ede38
kabradshaw1 <itllbok82@gmail.com>
2024-05-04 17:09:22 +0000

    working on the things

--- prisma/testDBSetup.sh
+++ prisma/testDBSetup.sh
@@ -1,23 +1,21 @@
-#!/blin/bash
-# Purpose: Create the test database so that my prisma tests can run
+#!/bin/bash
+# Purpose: Create the test database so that my prisma tests can run.  
+# Run this script with bash -x testDBSetup.sh
 
 # Variables
 DB_NAME="test_story_app"
 DB_USER="user"
 DB_PASSWORD="password"
-DB_HOST="postgresql"
-DB_PORT="5432"
+DB_HOST="localhost"
+DB_PORT="6000"
 DATABASE_URL="postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME"
 
 # Create the database
 echo "Creating database $DB_NAME..."
 psql -h $DB_HOST -U $DB_USER -c "CREATE DATABASE $DB_NAME;"
 
-# Set the environment variable for Prisma
-export DATABASE_URL=$DATABASE_URL
-
 # Run Prisma migrate
 echo "Running Prisma migrate..."
-npx prisma migrate deploy
+DATABASE_URL=$DATABASE_URL npx prisma migrate dev --name "test_setup"
 
 echo "Database setup complete."
--- prisma/tests/character.spec.ts
+++ prisma/tests/character.spec.ts
@@ -1,3 +1,5 @@
+import exp from "constants";
+
 import { Prisma } from "@prisma/client";
 
 import prisma from "../prismaClient";
@@ -14,7 +16,6 @@ describe("Character", () => {
 
   afterAll(async () => {
     try {
-      await prisma.character.deleteMany({});
       await prisma.$disconnect();
       console.log("Test database has been cleaned up");
     } catch (error) {
@@ -25,24 +26,58 @@ describe("Character", () => {
   describe("create", () => {
     afterEach(async () => {
       try {
+        // Delete records from all relevant tables
+        await prisma.role.deleteMany({});
+        await prisma.organization.deleteMany({});
         await prisma.character.deleteMany({});
+        await prisma.fileName.deleteMany({});
+
+        // Reset the ID sequences for all relevant tables
+        await prisma.$executeRaw`ALTER SEQUENCE "Organization_id_seq" RESTART WITH 1`;
+        await prisma.$executeRaw`ALTER SEQUENCE "Character_id_seq" RESTART WITH 1`;
+        await prisma.$executeRaw`ALTER SEQUENCE "Role_id_seq" RESTART WITH 1`;
+        await prisma.$executeRaw`ALTER SEQUENCE "FileName_id_seq" RESTART WITH 1`;
+        console.log("Delete existing characters after tests");
       } catch (error) {
         console.error(error);
       }
     });
+
     it("givenProperlyFormatted_whenCreate_thenCharacterCreated", async () => {
       // given
       const input: Prisma.CharacterCreateInput = {
         title: "title",
         text: "text",
+        fileNames: {
+          create: [{ fileName: "example.png", discriminator: "character" }],
+        },
+        role: {
+          create: [
+            {
+              title: "role",
+              organization: {
+                create: {
+                  title: "New Organization",
+                },
+              },
+            },
+          ],
+        },
       };
+
       // when
-      const character = await prisma.character.create({ data: input });
+      const character = await prisma.character.create({
+        data: input,
+        include: { fileNames: true, role: true },
+      });
+
       // then
       expect(character).toBeDefined();
-      expect(character.id).toHaveProperty("id");
+      expect(character.id).toBe(1);
       expect(character.title).toBe("title");
-      expect(character.text).toBe(null);
+      expect(character.text).toBe("text");
+      expect(character.fileNames).toBeDefined();
+      expect(character.fileNames[0].fileName).toBe("example.png");
       expect(character.createdAt).toBeInstanceOf(Date);
     });
   });
