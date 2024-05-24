f8bf39478be83e7394a58df08d888c26c69598e2
Kyle Bradshaw <itllbok82@gmail.com>
2024-05-23 23:38:54 -0400

    working on consistant enviroment

---
+++ app/(auth)/reset-password/page.tsx
@@ -0,0 +1,8 @@
+export default function ResetPassword() {
+  return (
+    <div>
+      <h1>Reset Password</h1>
+      <p>Reset password here</p>
+    </div>
+  );
+}
---
+++ components/example.tsx
@@ -0,0 +1,4 @@
+"use client";
+export default function Example() {
+  return <div>Example</div>;
+}
