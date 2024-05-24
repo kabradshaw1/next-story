cc5b4b6e32cabd60800a50e8ad42b9f2837627cf
Kyle Bradshaw <itllbok82@gmail.com>
2024-05-24 14:45:05 +0000

    working on consistant styling and linting

--- components/example.tsx
+++ components/example.tsx
@@ -1,4 +1,12 @@
-"use client";
-export default function Example() {
+'use client';
+
+import React from 'react';
+
+import { useQuery } from '@tanstack/react-query';
+
+export default function Example(): React.ReactElement {
+  const { data, error } = useQuery({
+    queryKey: ['example'],
+  });
   return <div>Example</div>;
 }
