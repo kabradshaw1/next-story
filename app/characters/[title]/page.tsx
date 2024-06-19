41293af85a56ddf3bfb08da09e1113f67d5a3f0f
Kyle Bradshaw <itllbok82@gmail.com>
2024-06-19 22:34:28 +0000

    working on the things

--- app/characters/[title]/page.tsx
+++ app/characters/[title]/page.tsx
@@ -1,13 +1,20 @@
-'use client';
-import { useQuery } from '@tanstack/react-query';
 import { gql } from 'graphql-tag';
-import { useParams } from 'next/navigation';
 
 import axiosInstance from '@/lib/axios';
 import { slugToTitle } from '@/lib/createSlug';
 
-const fetchCharacter = async (title: string): Promise<unknown> => {
-  console.log('fetching character', title);
+type Props = {
+  params: {
+    title: string;
+  };
+};
+
+export default async function SingleCharacterPage({
+  params,
+}: Props): Promise<JSX.Element> {
+  const { title: slug } = params;
+  const title = slugToTitle(slug);
+
   const query = gql`
     query character($title: String!) {
       character(title: $title) {
@@ -26,32 +33,9 @@ const fetchCharacter = async (title: string): Promise<unknown> => {
     }
   `;
 
-  try {
-    const response = await axiosInstance.post('', {
-      query: query.loc?.source.body,
-      variables: { title: 'Character 1' },
-    });
-    console.log('fetch data', response.data.data.character);
-    return response.data.data.character;
-  } catch (error) {
-    console.error('Error fetching character:', error);
-    throw error;
-  }
-};
-
-export default function SingleCharacterPage(): JSX.Element {
-  const { title: slug } = useParams<{ title: string }>();
-  const title = slugToTitle(slug);
-
-  const { data, error, isLoading } = useQuery({
-    queryKey: ['character', title],
-    queryFn: async () => await fetchCharacter(title),
+  const response = await axiosInstance.post('', {
+    query: query.loc?.source.body,
+    variables: { title },
   });
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error instanceof Error) return <div>Error: {error.message}</div>;
-
-  console.log('query data', data);
-
   return <div></div>;
 }
