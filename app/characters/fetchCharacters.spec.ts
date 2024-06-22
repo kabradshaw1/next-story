790318b2f107675906519c02af1af9a07ebdfbe8
Kyle Bradshaw <itllbok82@gmail.com>
2024-06-22 16:11:14 +0000

    working on tests

--- app/characters/fetchCharacters.spec.ts
+++ app/characters/fetchCharacters.spec.ts
@@ -6,11 +6,82 @@ import fetchCharacters from './fetchCharacters';
 
 describe('fetchCharacters', () => {
   let mock: MockAdapter;
+
   beforeEach(() => {
     mock = new MockAdapter(axiosInstance);
   });
 
   afterEach(() => {
     mock.restore();
+  });
+
+  it('should fetch and return characters with images', async () => {
+    const mockedResponse = {
+      data: {
+        data: {
+          characters: [
+            {
+              title: 'Character 1',
+              downloadURLs: [
+                'http://example.com/image1.jpg',
+                'http://example.com/image2.jpg',
+              ],
+            },
+            {
+              title: 'Character 2',
+              downloadURLs: ['http://example.com/image3.jpg'],
+            },
+          ],
+        },
+      },
+    };
+
+    mock.onPost('').reply(200, mockedResponse);
+
+    const characters = await fetchCharacters();
+
+    expect(characters).toEqual([
+      {
+        title: 'Character 1',
+        imageUrl: expect.stringMatching(
+          /http:\/\/example\.com\/image[12]\.jpg/
+        ),
+      },
+      { title: 'Character 2', imageUrl: 'http://example.com/image3.jpg' },
+    ]);
+  });
+
+  it('should handle empty downloadURLs', async () => {
+    const mockedResponse = {
+      data: {
+        data: {
+          characters: [
+            {
+              title: 'Character 1',
+              downloadURLs: [],
+            },
+            {
+              title: 'Character 2',
+              downloadURLs: null,
+            },
+          ],
+        },
+      },
+    };
+
+    mock.onPost('').reply(200, mockedResponse);
+
+    const characters = await fetchCharacters();
+
+    expect(characters).toEqual([
+      { title: 'Character 1', imageUrl: undefined },
+      { title: 'Character 2', imageUrl: undefined },
+    ]);
+  });
+
+  it('should throw an error for a failed API call', async () => {
+    mock.onPost('').reply(500);
+
+    await expect(fetchCharacters()).rejects.toThrow();
   });
 });
