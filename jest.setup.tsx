8f0ecf0018887c36f2db50626fda0c1fd39cf0f1
Kyle Bradshaw <itllbok82@gmail.com>
2024-06-23 02:24:09 +0000

    giving up on msw

--- jest.setup.tsx
+++ jest.setup.tsx
@@ -2,19 +2,7 @@ import '@testing-library/jest-dom';
 
 import Image, { type ImageProps } from 'next/image';
 
-import { server } from '@/mocks/node';
-
 Object.defineProperty(Image, 'default', {
   configurable: true,
   value: (props: ImageProps) => <Image {...props} alt={props.alt} />, // Simplified mock of the Next.js Image component
-});
-
-beforeAll(() => {
-  server.listen();
-});
-afterEach(() => {
-  server.resetHandlers();
-});
-afterAll(() => {
-  server.close();
 });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,19 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        "custom-blue": "#007bff",
-        "dark-gray": "#1a202c",
+        'custom-blue': '#007bff',
+        'dark-gray': '#1a202c',
       },
       fontFamily: {
-        orbitron: ["Orbitron", "sans-serif"],
+        orbitron: ['Orbitron', 'sans-serif'],
       },
     },
   },
