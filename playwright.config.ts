1ec1fa663a413fed6674a9d2d111cb9d17474563
Kyle Bradshaw <itllbok82@gmail.com>
2024-06-24 23:44:23 +0000

    working on the things

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,6 +6,8 @@ export default defineConfig({
   use: {
     baseURL: 'http://localhost:3000', // The base URL of your Next.js app
     headless: true, // Set to false if you want to see the browser action
+    browserName: 'chromium',
+
     screenshot: 'only-on-failure', // Take screenshots only on test failure
     video: 'retain-on-failure', // Record videos only on test failure
   },
