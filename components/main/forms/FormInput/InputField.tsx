638ae9f3163d54a94244dec0535bf2003072f246
Kyle Bradshaw <itllbok82@gmail.com>
2024-06-29 02:22:33 +0000

    working on the things

---
+++ components/main/forms/FormInput/InputField.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import type { UseFormRegister, FieldValues, Path } from 'react-hook-form';
+
+type InputFieldProps<T extends FieldValues> = {
+  id: Path<T>; // Ensure the id matches the correct type from react-hook-form
+  label: string;
+  placeholder: string;
+  error?: string;
+  register: UseFormRegister<T>;
+};
+
+const InputField = <T extends FieldValues>({
+  id,
+  label,
+  placeholder,
+  error,
+  register,
+}: InputFieldProps<T>): JSX.Element => {
+  return (
+    <div className="mb-4">
+      <label htmlFor={id} className="label">
+        {label}
+      </label>
+      <input
+        type="text"
+        id={id}
+        placeholder={placeholder}
+        className="input bg-gray-700"
+        {...register(id)}
+      />
+      {error !== null && <p className="error-message">{error}</p>}
+    </div>
+  );
+};
+export default InputField;
