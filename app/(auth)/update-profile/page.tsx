75feca8c275bbbe5560804348c52ee9dacafef4b
Kyle Bradshaw <itllbok82@gmail.com>
2024-06-29 20:03:34 +0000

    working on the things

---
+++ app/(auth)/update-profile/page.tsx
@@ -0,0 +1,3 @@
+export default function UpdateProfilePage(): JSX.Element {
+  return <div>Update Profile</div>;
+}
--- components/Header/Dropdown.tsx
+++ components/Header/Dropdown.tsx
@@ -1,8 +1,22 @@
 'use client';
+import Link from 'next/link';
 
 // import { useAppSelector } from '@/lib/store/store';
 
 export default function Dropdown(): JSX.Element {
   // const auth = useAppSelector((state) => state.auth);
-  return <p>Dropdown</p>;
+  return (
+    <div>
+      <p>Dropdown</p>
+      <Link className="dark-gray" href="/login">
+        Login
+      </Link>
+      <Link className="dark-gray" href="/register">
+        Register
+      </Link>
+      <Link className="dark-gray" href="/forgot-password">
+        Forgot Password
+      </Link>
+    </div>
+  );
 }
