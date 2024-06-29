f0b6ee71f95e893589a461d97216fd0b4167d9ff
Kyle Bradshaw <itllbok82@gmail.com>
2024-06-29 20:04:57 +0000

    styling

--- components/Header/NavBar/NavBar.tsx
+++ components/Header/NavBar/NavBar.tsx
@@ -1,33 +1,10 @@
-'use client';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 
 export default function NavBar(): JSX.Element {
-  const pathname = usePathname();
-
-  // Helper function to determine if the link is active
-  const isActive = (path: string): boolean => pathname === path;
   return (
     <nav className="flex justify-start items-center py-4">
-      <Link
-        className={isActive('/') ? 'font-bold custom-blue' : 'dark-gray'}
-        href="/"
-      >
+      <Link className="dark-gray" href="/">
         Galaxy Voyagers
-      </Link>
-      <Link
-        className={isActive('/login') ? 'font-bold custom-blue' : 'dark-gray'}
-        href="/login"
-      >
-        Login
-      </Link>
-      <Link
-        className={
-          isActive('/register') ? 'font-bold custom-blue' : 'dark-gray'
-        }
-        href="/register"
-      >
-        Register
       </Link>
     </nav>
   );
