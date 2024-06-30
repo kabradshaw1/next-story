8782c9b945fc73fd6824d5a3ca61d8f2bdf0c4af
Kyle Bradshaw <itllbok82@gmail.com>
2024-06-30 21:19:15 +0000

    done refactoring for now.  Starting on Orgs

--- app/(story)/characters/layout.tsx
+++ app/(story)/characters/layout.tsx
@@ -1,23 +1,9 @@
-import Logo from '@/components/Logo/Logo';
+import StoryLayout from '@/components/StoryLayout/StoryLayout';
 
 export default function CharacterLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>): JSX.Element {
-  return (
-    <div className="container mx-auto p-4 bg-dark-gray">
-      <div className="grid grid-cols-1 lg:grid-cols-[1fr_3fr] gap-4">
-        <div className="hidden lg:block">
-          <Logo />
-          <p>
-            The characters in galaxy voyagers are all made by fans. Anyone can
-            create a character. The characters will be made cannon, based on
-            their popularity.
-          </p>
-        </div>
-        <div>{children}</div>
-      </div>
-    </div>
-  );
+  return <StoryLayout routeName="character">{children}</StoryLayout>;
 }
