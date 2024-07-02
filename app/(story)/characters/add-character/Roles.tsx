c4b390962a59696436888a08e178780ceac474d5
Kyle Bradshaw <itllbok82@gmail.com>
2024-07-02 17:12:06 +0000

    I love it

--- app/(story)/characters/add-character/Roles.tsx
+++ app/(story)/characters/add-character/Roles.tsx
@@ -1,6 +1,7 @@
 'use client';
 import type { Dispatch, SetStateAction } from 'react';
 
+import CheckBoxList from '@/components/CheckBoxList/CheckBoxList';
 import { useOrganizationsQuery } from '@/generated/graphql';
 
 type RolesProps = {
@@ -38,42 +39,11 @@ export default function Roles({
       return acc.concat(processedRoles);
     }, [] as ProcessedRoles) ?? [];
 
-  const handleRoleChange = (roleId: number): void => {
-    setSelectedRoles((prevSelectedRoles) => {
-      if (prevSelectedRoles.includes(roleId)) {
-        return prevSelectedRoles.filter((id) => id !== roleId);
-      } else {
-        return [...prevSelectedRoles, roleId];
-      }
-    });
-  };
-
   return (
-    <>
-      {roles?.map((role) => (
-        <div key={role?.id} className="mb-2">
-          <label className="inline-flex items-center">
-            <input
-              type="checkbox"
-              className="form-checkbox"
-              value={role?.id ?? undefined}
-              checked={
-                role?.id !== undefined &&
-                role?.id !== null &&
-                selectedRoles.includes(role.id)
-              }
-              onChange={() => {
-                if (role?.id !== undefined && role?.id !== null) {
-                  handleRoleChange(role.id);
-                }
-              }}
-            />
-            <span className="ml-2">
-              {role?.title} of {role?.organization}
-            </span>
-          </label>
-        </div>
-      ))}
-    </>
+    <CheckBoxList
+      data={roles}
+      selected={selectedRoles}
+      setSelected={setSelectedRoles}
+    />
   );
 }
---
+++ components/CheckBoxList/CheckBoxList.spec.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import { render, screen, fireEvent } from '@testing-library/react';
+
+import CheckBoxList from './CheckBoxList';
+
+describe('CheckBoxList Component', () => {
+  const setSelectedMock = jest.fn();
+  const data = [
+    { title: 'Item 1', id: 1 },
+    { title: 'Item 2', id: 2 },
+    { title: 'Item 3', id: 3 },
+  ];
+  const selected = [1, 3];
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('renders checkboxes correctly', () => {
+    render(
+      <CheckBoxList
+        data={data}
+        selected={selected}
+        setSelected={setSelectedMock}
+      />
+    );
+
+    const checkboxes = screen.getAllByRole('checkbox');
+    expect(checkboxes).toHaveLength(data.length);
+    expect(checkboxes[0]).toBeChecked();
+    expect(checkboxes[1]).not.toBeChecked();
+    expect(checkboxes[2]).toBeChecked();
+  });
+
+  test('handles checkbox change correctly', () => {
+    render(
+      <CheckBoxList
+        data={data}
+        selected={selected}
+        setSelected={setSelectedMock}
+      />
+    );
+
+    const checkboxes = screen.getAllByRole('checkbox');
+
+    // Uncheck the first checkbox
+    fireEvent.click(checkboxes[0]);
+    expect(setSelectedMock).toHaveBeenCalledWith(expect.any(Function));
+    let updateFunction = setSelectedMock.mock.calls[0][0];
+    expect(updateFunction(selected)).toEqual([3]);
+
+    // Check the second checkbox
+    fireEvent.click(checkboxes[1]);
+    expect(setSelectedMock).toHaveBeenCalledWith(expect.any(Function));
+    updateFunction = setSelectedMock.mock.calls[1][0];
+    expect(updateFunction(selected)).toEqual([1, 3, 2]);
+
+    // Uncheck the third checkbox
+    fireEvent.click(checkboxes[2]);
+    expect(setSelectedMock).toHaveBeenCalledWith(expect.any(Function));
+    updateFunction = setSelectedMock.mock.calls[2][0];
+    expect(updateFunction(selected)).toEqual([1]);
+  });
+});
